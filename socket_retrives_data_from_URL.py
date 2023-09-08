1b221f60448513dbcdd6a447bbfe578759f773e1
Roy Contreras Benitez <roy7474@gmail.com>
2023-09-07 21:42:03 -0400

    Try and except have been added along with option for user to enter the URL

--- socket_retrives_data_from_URL.py
+++ socket_retrives_data_from_URL.py
@@ -4,13 +4,17 @@ Don't worry about the headers for this exercise,
 simply show the first 3000 characters of the document contents.”'''
 
 import urllib.request, urllib.parse, urllib.error
-
-# retrieving the document from a URL
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo-full.txt')
 count = 0
+# Testing side link: http://data.pr4e.org/romeo-full.txt
+user_file = input('Enter the URL of the website that you would like to use: ')
+try:
+    fhand =urllib.request.urlopen(user_file) # retrieving the document from a URL
+except:
+    print("There was an error while trying to open this file. Please try again! ")
+
 for line in fhand:
     words = line#.split()
     count = count + len(words) #counting the characters
     if count < 3000:
-        print(line.decode().strip())
-print('the total number of character is: ', count)
+        print(line.decode().strip()) #displaying only 3000 characters
+print('The total number of character is:', count)
