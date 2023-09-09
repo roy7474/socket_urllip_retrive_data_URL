eb887005e375ac9122e102cce652ec65ef069189
Roy Contreras Benitez <roy7474@gmail.com>
2023-09-08 22:25:59 -0400

    BS4 has been imported and other small changes have been made

---
+++ socket_paragraph_counter.py
@@ -0,0 +1,39 @@
+'''
+Change the urllinks.py program to extract and count paragraph (p) tags from the retrieved HTML 
+document and display the count of the paragraphs as the output of your program. 
+Do not display the paragraph text, only count them. Test your program on several
+ small web pages as well as some larger web pages”
+    
+    
+    #PREVIOUS PROGRAM
+
+ import urllib.request, urllib.parse, urllib.error
+count = 0
+# Testing side link: http://data.pr4e.org/romeo-full.txt
+user_file = input('Enter the URL of the website that you would like to use: ')
+try:
+    fhand =urllib.request.urlopen(user_file) # retrieving the document from a URL
+    
+except:
+    print("There was an error while trying to open this file. Please try again! ")
+
+for line in fhand:
+    words = line#.split()
+    count = count + len(words) #counting the characters
+    if count < 3000:
+        print(line.decode().strip()) #displaying only 3000 characters
+print('The total number of character is:', count)
+'''
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+url = 'http://data.pr4e.org/romeo-full.txt'
+response = urllib.request.urlopen(url)
+html = response.read()
+#parse the html
+soup = BeautifulSoup(html, 'html.parser')
+
+paragraphs = soup.find_all(['p', 'div'])
+num_paragraphs = len(paragraphs)
+
+print(soup)
+print("The number of paragraphs tags are: ", num_paragraphs)
