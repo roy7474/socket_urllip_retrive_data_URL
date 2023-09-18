56a971e93a322a320b431739a0de0f0a3e15e650
Roy Contreras Benitez <roy7474@gmail.com>
2023-09-17 20:53:06 -0400

    try and except have been added to ask user to enter the website

--- socket_paragraph_counter.py
+++ socket_paragraph_counter.py
@@ -41,20 +41,25 @@ https://en.wikipedia.org/wiki/History_of_Python
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+user_url = input('Please enter the URL of the website to count its parragraphs: ')
+try :
+    url = user_url
+    html = urllib.request.urlopen(url, context = ctx).read()
 
-url = 'https://en.wikipedia.org/wiki/History_of_Python'
-html = urllib.request.urlopen(url, context = ctx).read()
+    #parse the html
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('p')
+    counts = 0
+    for tag in tags:
+        counts += 1
 
-#parse the html
-soup = BeautifulSoup(html, 'html.parser')
-tags = soup('p')
-counts = 0
-for tag in tags:
-    counts += 1
+except NameError:
+    print('There was an error while trying to open the URL that you provided. Please try again!')
+except ValueError:
+    print('There was an error while trying to open the URL that you provided. Please try again!')
 
 #paragraphs = soup.find_all(['p', 'div'])
 #num_paragraphs = len(paragraphs)
 #print(num_paragraphs)
 
 print("The number of paragraphs tags are: ", counts)
-
